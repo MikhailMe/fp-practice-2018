b9285d1a40cfaa65a463c5cc85fb340595961dd4
MikhailMe <neon-pf@yandex.ru>
2018-10-27 15:42:35 +0300

    finished Task2_2

--- Task2_2.hs
+++ Task2_2.hs
@@ -5,12 +5,10 @@ import Todo(todo)
 import Prelude hiding (foldl, foldr, unfoldr, map, concatMap, 
     filter, maxBy, minBy, reverse, sum, product, elem)
 
--- foldl f s [a1, a2, a3, a4] = (((s `f` a1) `f` a2) `f` a3) `f` a4
 foldl :: (b -> a -> b) -> b -> [a] -> b
 foldl f x []     = x
 foldl f x (y:ys) = foldl f (f x y) ys
 
--- foldr f s [a1, a2, a3, a4] = a1 `f` (a2 `f` (a3 `f` (a4 `f` s)))
 foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr f x []     = x
 foldr f x (y:ys) = f y (foldr f x ys)
@@ -42,7 +40,7 @@ catMaybes = foldr (\ item lst -> case item of
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = 	todo
+diagonal a = unfoldr (\ i -> if i + 1 > length a then Nothing else Just(a !! i !! i, i + 1)) 0
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
@@ -56,7 +54,7 @@ elem x = foldl (\ s x' -> x' == x || s) False
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
 rangeTo from to step 
  | from > to = error "error: from > to" 
- | otherwise = unfoldr (\ from -> if (from + 1) > to then Nothing else Just(from, from + step)) from
+ | otherwise = unfoldr (\ from -> if from + 1 > to then Nothing else Just(from, from + step)) from
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
@@ -65,17 +63,17 @@ append first second = foldr (:) second first
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = unfoldr (\ lst -> Just(lst, drop n lst)) (take n lst)
+groups lst n = takeWhile (not.null) $ unfoldr (Just . splitAt (fromIntegral n)) lst
 
--- unfoldr f = maybe [] (\ (a, b) -> a : unfoldr f b) . f
+-- второй вариант для "groups" - более длинный, но делает тоже самое 
 
-group :: Int -> [a] -> [[a]]
-group _ [] = []
-group n l
-  | n > 0 = (take n l) : (group n (drop n l))
-  | otherwise = error "Negative n"
+{- 
+takeN :: Integer -> [a] -> [a]
+takeN n l = take (fromIntegral n) l
 
--- *Task2_2> take n a
--- [1,2,3,4,5]
--- *Task2_2> drop n a
--- [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+dropN :: Integer -> [a] -> [a]
+dropN n l = drop (fromIntegral n) l
+
+groups2 :: [a] -> Integer -> [[a]]
+groups2 lst n = takeWhile (not.null) $ unfoldr (\ lst -> Just(takeN n lst, dropN n lst)) lst
+-}
