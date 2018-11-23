fd3fe2d7d662018833fbbf0d10e2207b8a48edf6
MikhailMe <neon-pf@yandex.ru>
2018-11-23 03:29:46 +0300

    finished Task3_2

--- Task3_2.hs
+++ Task3_2.hs
@@ -5,27 +5,41 @@ import Todo(todo)
 data ReverseList a = RNil | RCons (ReverseList a) a
 
 rlistToList :: ReverseList a -> [a]
-rlistToList = todo
+rlistToList RNil               = []
+rlistToList (RCons next value) = value : (rlistToList next)
 
 listToRList :: [a] -> ReverseList a
-listToRList = todo
+listToRList []    = RNil
+listToRList (h:t) = RCons (listToRList t) h
 
 -- Реализуйте классы Eq, Ord, Show, Monoid, Functor
 
-instance (Show a) => Show (ReverseList a) where
-    show RNil = "[]"
-
 instance (Eq a) => Eq (ReverseList a) where
-    (==) rl1 rl2 = todo
+    (==) RNil RNil                                 = True
+    (==) RNil _                                    = False
+    (==) _ RNil                                    = False
+    (==) (RCons next1 value1) (RCons next2 value2) = next1 == next2 && value2 == value2
 
 instance (Ord a) => Ord (ReverseList a) where
-    (<=) rl1 rl2 = todo
+    (<=) RNil RNil                                 = True
+    (<=) RNil _                                    = True
+    (<=) _ RNil                                    = False
+    (<=) (RCons next1 value1) (RCons next2 value2) = value1 <= value2 || next1 <= next2
 
-instance Semigroup (ReverseList a) where
-    (<>) = todo
-
+instance (Show a) => Show (ReverseList a) where
+    show RNil               = "[]"
+    show (RCons RNil value) = show value
+    show (RCons next value) = show next ++ "," ++ show value
+  
 instance Monoid (ReverseList a) where
-    mempty = todo
+    mempty = RNil
+
+    mappend RNil list               = list
+    mappend list RNil               = list
+    mappend list (RCons next value) = RCons (mappend list next) value
+
+    mconcat = foldr mappend mempty
 
 instance Functor ReverseList where
-    fmap = todo
+    fmap _ RNil                      = RNil
+    fmap function (RCons next value) = RCons (fmap function next) (function value)
